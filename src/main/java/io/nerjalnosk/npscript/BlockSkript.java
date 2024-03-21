5b01f1a858cf02095640b2c2a6d9376ada79ee05
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-03-21 16:57:40 +0100

    Initial commit

---
+++ src/main/java/io/nerjalnosk/npscript/BlockSkript.java
@@ -0,0 +1,10 @@
+package io.nerjalnosk.npscript;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BlockSkript implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        //
+    }
+}
---
+++ src/main/java/io/nerjalnosk/npscript/client/BlockSkriptClient.java
@@ -0,0 +1,10 @@
+package io.nerjalnosk.npscript.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockSkriptClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        //
+    }
+}
